ee4462f01be3775af7e6d6e5c85498939ac5119b
VINIX019 <73047364+VINIX019@users.noreply.github.com>
2026-05-12 08:32:05 -0300

    Atualizar o Fornecedor.java

--- estoque/src/Fornecedor.java
+++ estoque/src/Fornecedor.java
@@ -13,14 +13,12 @@ public class Fornecedor {
         this.email = email;
     }
 
-    // Getters
     public int getId() { return id; }
     public String getNome() { return nome; }
     public String getCnpj() { return cnpj; }
     public String getTelefone() { return telefone; }
     public String getEmail() { return email; }
 
-    // Setters
     public void setNome(String nome) { this.nome = nome; }
     public void setCnpj(String cnpj) { this.cnpj = cnpj; }
     public void setTelefone(String telefone) { this.telefone = telefone; }
